2ca8f6f447c2e005cb7e2b7e262578f49ab57a6e
martuky <marta.vina.rico@gmail.com>
2025-08-30 17:09:57 +0200

    solve error change user role

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -9,14 +9,20 @@ import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { META_ROLES } from 'src/auth/decorators/role-protected.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  canActivate(
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): Promise<boolean> {
     const validRoles: string[] = this.reflector.get(
       META_ROLES,
       context.getHandler(),
@@ -27,9 +33,14 @@ export class UserRoleGuard implements CanActivate {
 
     const req = context.switchToHttp().getRequest();
     const user = req.user as User;
-
+    const userRep = await this.userRepository.findOne({
+      where: { id: user.id },
+      select: ['roles'], // Ajusta los campos que deseas incluir
+    });
+    
+    const roles = userRep.roles;
     if (!user) throw new BadRequestException('User not found');
-    for (const role of user.roles) {
+    for (const role of roles) {
       if (validRoles.includes(role)) {
         return true;
       }
