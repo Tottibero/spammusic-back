e95b71d08b61142308adc3ed35fffefb9c93b5de
martavina <marta.vina.rico@gmail.com>
2026-01-05 08:17:59 +0100

    feat(logs): modify solarwinds logs and create jobs to run the schedulers

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { WinstonModule, utilities as nestWinstonUtilities } from 'nest-winston';
-import * as winston from 'winston';
-import 'winston-daily-rotate-file';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './winston.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.ms(),
-            nestWinstonUtilities.format.nestLike('SpamMusic', {
-              prettyPrint: true,
-              colors: true,
-            }),
-          ),
-        }),
-        new winston.transports.DailyRotateFile({
-          filename: 'logs/error-%DATE%.log',
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          level: 'error',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(),
-          ),
-        }),
-        new winston.transports.DailyRotateFile({
-          filename: 'logs/combined-%DATE%.log',
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(),
-          ),
-        }),
-        new winston.transports.Http({
-          host: 'logs.collector.eu-01.cloud.solarwinds.com',
-          path: '/v1/logs',
-          ssl: true,
-          headers: {
-            Authorization: `Bearer fDK3DNFpJspyevqaNj8qev2rBzAML9Ha6eHt8G5tKbOZ8IQ5TM6RKNk4H_gy36q3lHXSct0`,
-          },
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(),
-          ),
-        }),
-      ],
-    }),
+    logger: WinstonModule.createLogger(winstonConfig),
   });
 
   app.setGlobalPrefix('api');
---
+++ src/winston.config.ts
@@ -0,0 +1,53 @@
+import { utilities as nestWinstonUtilities } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
+export const winstonConfig = {
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                nestWinstonUtilities.format.nestLike('SpamMusic', {
+                    prettyPrint: true,
+                    colors: true,
+                }),
+            ),
+        }),
+        new winston.transports.DailyRotateFile({
+            filename: 'logs/error-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+            level: 'error',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.DailyRotateFile({
+            filename: 'logs/combined-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.Http({
+            host: 'logs.collector.eu-01.cloud.solarwinds.com',
+            path: '/v1/logs',
+            ssl: true,
+            headers: {
+                Authorization: `Bearer fDK3DNFpJspyevqaNj8qev2rBzAML9Ha6eHt8G5tKbOZ8IQ5TM6RKNk4H_gy36q3lHXSct0`,
+            },
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+        }),
+    ],
+};
